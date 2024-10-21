594f478cccd181c386b810836c013bb9a7efe12a
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2024-10-21 09:11:59 +0200

    add supermaven -- works

--- init.lua
+++ init.lua
@@ -256,6 +256,13 @@ require('lazy').setup({
     },
   },
 
+  {
+    'supermaven-inc/supermaven-nvim',
+    config = function()
+      require('supermaven-nvim').setup {}
+    end,
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
@@ -835,7 +842,8 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'vim'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
